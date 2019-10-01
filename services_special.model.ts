24f45caf58fa6b23c1e431fc5040955f053812b0
kundansgupta <kundansgupta@gmail.com>
2019-10-01 16:28:10 +1000

    Create services_special.model.ts

---
+++ services_special.model.ts
@@ -0,0 +1,26 @@
+export class SpecialModel {
+    Id: number;
+    Name: string;
+    Age: string;
+    Gender: string;
+    Area: string;
+    City: string;
+    Sector: string;
+    Kshetra: string;
+    Contact1: string;
+    Contact2: string;
+    PersonalNumber: string;
+    Attendance: string;
+    SewaLocation: string;
+    Status: string;
+    IsRegistered: string;
+    AvailableOn: string;
+    PassNumber: string;
+    IsActive: boolean;
+    AddedDate: Date;
+    ModifiedDate: Date;
+    IsDeleted: boolean;
+    UnitNo: string;
+    IsEdit: boolean;
+    IsNew: boolean;
+}
